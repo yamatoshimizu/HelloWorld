3d9280a1c5b5b56f97ef27925ae955152a3b05c7
Yamato Shimizu <yamato.shimiu.0629@gmail.com>
2020-07-01 10:59:13 +0900

    Initial Commit

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by 清水大和 on 2020/07/01.
+//  Copyright © 2020 Yamato Shimizu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
